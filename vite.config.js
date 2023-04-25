3029b987f3e5142256b22a8cb36b63c76f2780c6
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-04-25 17:11:46 +0600

    Exclude .env file from Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    fs: {
+      allow: ['.']
+    },
+    watch: {
+      ignored: [
+        'node_modules/**',
+        '.git/**',
+        '**/.env' // add this line to ignore .env file
+      ]
+    }
+  }
 })
